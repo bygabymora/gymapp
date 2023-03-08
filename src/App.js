9ddcae0ffeced9396d7f94f72d651b4f32e5a082
bygabymora <bygabymora@gmail.com>
2023-03-08 10:15:32 -0500

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -11,9 +11,9 @@ import Footer from './components/Footer';
 const App = () => (
   <Box width="400px" sx={{ width: { xl: '1488px' } }} m="auto">
     <Navbar />
+    <Home/>
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/exercise/:id" element={<ExerciseDetail />} />
+        <Route path="/exercise/:id" element={<ExerciseDetail />} />
     </Routes>
     <Footer />
   </Box>
